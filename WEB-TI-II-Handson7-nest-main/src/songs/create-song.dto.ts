8c1a1da14bdb74f5faf9ba76f27dffe4375fdc00
Andre Lumbantoruan <2381031@unai.edu>
2025-03-21 11:15:42 +0700

    commit

--- WEB-TI-II-Handson7-nest-main/src/songs/create-song.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreateSongDTO{
-    title : string;
-    artist : string;
-}
---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './logger.middleware';
+import { AuthGuard } from './auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    {
+    provide: APP_GUARD,
+    useClass: AuthGuard,
+  },AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
+  }
+
+}
